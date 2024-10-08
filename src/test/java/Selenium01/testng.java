6a251bcc9459e53f6d3314ae107432430cbbd177
sivaramakrishnakarra <sivaramakrishnakarra@macbookair>
2024-10-08 18:08:09 +0100

    testng changes

--- src/test/java/Selenium01/testng.java
+++ src/test/java/Selenium01/testng.java
@@ -1,8 +1,12 @@
 package Selenium01;
 
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver.Timeouts;
 
 
 public class testng {
@@ -10,12 +14,20 @@ public class testng {
 	
 	@Test
 	
-	public void test01() {
+	public void test01() throws InterruptedException {
 		System.out.println("testng is working");
 		
 		WebDriver driver = new ChromeDriver();
 		
 		driver.get("https://demoqa.com/alerts");
+		
+		driver.manage().window().maximize();
+		
+		driver.manage().timeouts().wait(300);
+		
+		driver.findElement(By.xpath("//button[@id=\"alertButton\"]")).click();
+		
+		driver.switchTo().alert().accept();
 		
 	}
 
